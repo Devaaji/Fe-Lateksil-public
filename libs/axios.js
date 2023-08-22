ffcd36f495be57e34246aea2cc8eb213805b98c2
Devaaji <devaaji5@gmail.com>
2023-08-22 08:49:27 +0700

    [CHANGE] baseURL

--- libs/axios.js
+++ libs/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { parseCookies } from 'nookies';
 import useAuthUserStore from '../store/useAuthUserStore';
 
-export const baseUrl = 'http://192.168.60.168:3030/';
+export const baseUrl = 'https://lateksil.xyz/';
 
 const axiosInstance = axios.create({
   baseURL: baseUrl,
